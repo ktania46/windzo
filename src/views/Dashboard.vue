ba523ab41e3e1229eb8bbffb83919f49609a6a56
Tania Kapoor <ktania46@gmail.com>
2022-05-15 22:10:48 +0200

    Data Tables, Reports

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -286,45 +286,215 @@
       </div>
     </div>
   </div>
+  <div class="m-4">
+    <table-lite
+      :is-loading="table.isLoading"
+      :columns="table.columns"
+      :rows="table.rows"
+      :total="table.totalRecordCount"
+      :sortable="table.sortable"
+      :messages="table.messages"
+      @do-search="doSearch"
+      @is-finished="table.isLoading = false"
+    />
+  </div>
+  <div class="m-4">
+    <table-lite
+      :is-loading="table1.isLoading"
+      :columns="table1.columns"
+      :rows="table1.rows"
+      :total="table1.totalRecordCount"
+      :sortable="table1.sortable"
+      :messages="table1.messages"
+      @do-search="doSearch1"
+      @is-finished="table1.isLoading = false"
+    />
+  </div>
 </template>
 
 <script>
-  // @ is an alias to /src
-  import { Icon } from "@iconify/vue";
+// @ is an alias to /src
+import { Icon } from "@iconify/vue";
+import { reactive } from "vue";
+import TableLite from "vue3-table-lite";
 
-  export default {
-    name: "Dashboard",
-    data() {
-      return {
-        // for more guide apexchart.js
-        // https://apexcharts.com/docs/chart-types/line-chart/
+// Fake Data for 'asc' sortable
+const sampleData1 = (offst, limit) => {
+  offst = offst + 1;
+  let data = [];
+  for (let i = offst; i <= limit; i++) {
+    data.push({
+      id: i,
+      name: "TEST" + i,
+      email: "test" + i + "@example.com",
+    });
+  }
+  return data;
+};
 
-        // chart data line
-        optionsLine: {
-          chart: {
-            id: "vuechart-example",
-          },
-          xaxis: {
-            categories: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
-          },
+// Fake Data for 'desc' sortable
+const sampleData2 = (offst, limit) => {
+  let data = [];
+  for (let i = limit; i > offst; i--) {
+    data.push({
+      id: i,
+      name: "TEST" + i,
+      email: "test" + i + "@example.com",
+    });
+  }
+  return data;
+};
+
+export default {
+  name: "Dashboard",
+  setup() {
+    // Table config
+    const table = reactive({
+      isLoading: false,
+      columns: [
+        {
+          label: "ID",
+          field: "id",
+          width: "3%",
+          sortable: true,
+          isKey: true,
         },
-        colors: ["#16A34A"],
-        stroke: {
-          curve: "smooth",
+        {
+          label: "Name",
+          field: "name",
+          width: "10%",
+          sortable: true,
         },
-        seriesLine: [
-          {
-            name: "series-1",
+        {
+          label: "Email",
+          field: "email",
+          width: "15%",
+          sortable: true,
+        },
+      ],
+      rows: [],
+      totalRecordCount: 0,
+      sortable: {
+        order: "id",
+        sort: "asc",
+      },
+    });
+    const table1 = reactive({
+      isLoading: false,
+      columns: [
+        {
+          label: "ID",
+          field: "id",
+          width: "3%",
+          sortable: true,
+          isKey: true,
+        },
+        {
+          label: "Name",
+          field: "name",
+          width: "10%",
+          sortable: true,
+        },
+        {
+          label: "Email",
+          field: "email",
+          width: "15%",
+          sortable: true,
+        },
+      ],
+      rows: [],
+      totalRecordCount: 0,
+      sortable: {
+        order: "id",
+        sort: "asc",
+      },
+    });
 
-            data: [30, 40, 45, 50, 49, 60, 70, 91],
-          },
-        ],
-      };
-      // end chart data line
-    },
-    components: {
-      Icon,
-    },
-    mounted() {},
-  };
+    /**
+     * Search Event
+     */
+    const doSearch = (offset, limit, order, sort) => {
+      console.log("Ekas")
+      table.isLoading = true;
+      setTimeout(() => {
+        table.isReSearch = offset == undefined ? true : false;
+        if (offset >= 10 || limit >= 20) {
+          limit = 20;
+        }
+        if (sort == "asc") {
+          table.rows = sampleData1(offset, limit);
+        } else {
+          table.rows = sampleData2(offset, limit);
+        }
+        table.totalRecordCount = 20;
+        table.sortable.order = order;
+        table.sortable.sort = sort;
+      }, 600);
+    };
+    const doSearch1 = (offset, limit, order, sort) => {
+      console.log("Ekas1")
+      table1.isLoading = true;
+      setTimeout(() => {
+        table1.isReSearch = offset == undefined ? true : false;
+        if (offset >= 10 || limit >= 20) {
+          limit = 20;
+        }
+        if (sort == "asc") {
+          table1.rows = sampleData1(offset, limit);
+        } else {
+          table1.rows = sampleData2(offset, limit);
+        }
+        table1.totalRecordCount = 20;
+        table1.sortable.order = order;
+        table1.sortable.sort = sort;
+      }, 600);
+    };
+
+    // First get data
+    doSearch(0, 10, "id", "asc");
+
+    // First get data
+    doSearch1(0, 10, "id", "asc");
+
+    return {
+      table,
+      table1,
+      doSearch,
+      doSearch1
+    };
+  },
+  data() {
+    return {
+      // for more guide apexchart.js
+      // https://apexcharts.com/docs/chart-types/line-chart/
+
+      // chart data line
+      optionsLine: {
+        chart: {
+          id: "vuechart-example",
+        },
+        xaxis: {
+          categories: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
+        },
+      },
+      colors: ["#16A34A"],
+      stroke: {
+        curve: "smooth",
+      },
+      seriesLine: [
+        {
+          name: "series-1",
+
+          data: [30, 40, 45, 50, 49, 60, 70, 91],
+        },
+      ],
+    };
+    // end chart data line
+  },
+  components: {
+    Icon,
+    TableLite,
+  },
+  mounted() {},
+};
 </script>
